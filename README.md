ee416f49fb9ea152c182795d4e77dcd688c9e840
BrielleCarvalho <brielle.127@gmail.com>
2019-05-08 22:12:47 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PositiveNewsScraper
+
+## Scraping a positive news source to populate a list of Articles and allows users to comment on articles.
---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Exporting an object containing all of our models
+
+module.exports = {
+    Story: require("./Article"),
+    Note: require("./Note")
+  };
+  
